de8b14be09518951fb7d31e8cb46c68e2032a0d1
khrahat92 <khrahat92@gmail.com>
2023-06-25 12:45:08 +0600

    change

--- src/App.js
+++ src/App.js
@@ -42,11 +42,11 @@ function App() {
   useEffect(() => {
     if (timerArr.length === 0) return;
     console.log("Hi world, Hello, ", timerArr[0]);
+    videoRef.current.onloadstart = () => { 
+      console.log('loading video')
+      videoRef.current.currentTime = timerArr[0];
+    }
     if (time && timerArr[0]) {
-      videoRef.current.onloadstart = () => { 
-        console.log('loading video')
-        videoRef.current.currentTime = timerArr[0];
-      }
       videoRef.current.currentTime = timerArr[0];
     }
   }, [timerArr[0], time]);
