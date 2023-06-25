407ddfc77111c1fd24a46643326d14226a3c56c0
khrahat92 <khrahat92@gmail.com>
2023-06-25 13:00:47 +0600

    change

--- src/App.js
+++ src/App.js
@@ -42,11 +42,13 @@ function App() {
   useEffect(() => {
     if (timerArr.length === 0) return;
     console.log("Hi world, Hello, ", timerArr[0]);
-    videoRef.current.onloadstart = () => { 
-      console.log('i want time', timerArr[0])
-      const recentTime = timerArr[0];
-      console.log('loading video')
-      videoRef.current.currentTime = recentTime
+    if (time) {
+      videoRef.current.onloadstart = () => {
+        console.log("i want time", timerArr[0]);
+        const recentTime = timerArr[0];
+        console.log("loading video");
+        videoRef.current.currentTime = recentTime;
+      };
     }
     
   }, [timerArr[0], time]);
